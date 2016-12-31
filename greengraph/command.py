352e42655fc5f3b293320d34243cd1eaaa7285f8
Jan <jan.zmazek@gmail.com>
2016-12-31 13:15:04 +0100

    Fixed minor bugs, so the command line script works.

--- greengraph/command.py
+++ greengraph/command.py
@@ -4,10 +4,10 @@ from .makeGraph import MakeGraph # Note python 3 relative import
 def process():
 	parser = ArgumentParser(description = "Generate a plot of green pixels intensity between two locations.")
 
-	parser.add_argument('--from', '-f', help='start location', type='str')
-	parser.add_argument('--to', '-t', help='end location', type='str')
-	parser.add_argument('--steps', '-s', help='Number of steps between locations', type='int')
-	parser.add_argument('--out', '-o', help='Output file name (e.g. plot.png)', type='str')
+	parser.add_argument('--from', '-f', help='start location', type=str, dest="start")
+	parser.add_argument('--to', '-t', help='end location', type=str, dest="end")
+	parser.add_argument('--steps', '-s', help='Number of steps between locations', type=int, dest="steps")
+	parser.add_argument('--out', '-o', help='Output file name (e.g. plot.png)', type=str, dest="out")
 
 	arguments= parser.parse_args()
 

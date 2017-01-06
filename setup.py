53116c8a060a329f954eb26e1a35e3ec5c8da14a
Jan <jan.zmazek@gmail.com>
2017-01-06 12:43:43 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Greengraph",
-    version = "0.1.0",
+    version = "1.0.0",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/greengraph'],
     install_requires = ['argparse', 'numpy', 'geopy', 'matplotlib', 'requests']
